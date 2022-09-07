5ef081dde546e280fa8383ca01eba9a0d311eb06
Sim0nHack <simonhack.wdpt22@protonmail.com>
2022-09-07 16:13:53 +0200

    Iteration 14: <Facebook /> (advanced)

--- src/component/Facebook.js
+++ src/component/Facebook.js
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import profiles from '../data/berlin.json';
 import Profile from './Profile';
 
-// {
-//     "firstName": "Amelia",
-//     "lastName": "Chorng Ua",
-//     "country": "Malaysia",
-//     "img": "http://res.cloudinary.com/djyjdargg/image/upload/v1550168765/Ironclass/IH_Jan1952.jpg.jpg",
-//     "isStudent": true
-// }
-
 function Facebook() {
+    const [countryHighlight, setCountryHighlight] = useState("");
+
     const styles = {
+        selectBar: {
+            display:        "flex",
+            flexWrap:       "wrap",
+            width:          "40vw",
+            margin:         10,
+            gap:            10,
+        },
+        button: {
+            display:        "flex",
+            justifyContent: "center",
+            alignItems:     "center",
+            border:         "1px solid black",
+            padding:        5
+        },
         list: {
             display:        "flex",
             flexDirection:  "column",
@@ -19,16 +28,53 @@ function Facebook() {
         }
     };
 
+    // Extracting the list of countries from "profiles" data
+    const countries = profiles.reduce((countries, user) => countries.includes(user.country) ? countries : [...countries, user.country], []);
+
+    function highlight(country) {
+        setCountryHighlight(country);
+    }
+
     return (
-        <div style={styles.list}>
-            {profiles.map(profile => <Profile
-                                        firstName={profile.firstName}
-                                        lastName={profile.lastName}
-                                        img={profile.img}
-                                        country={profile.country}
-                                        isStudent={profile.isStudent} />)}
+        <div>
+            <div style={styles.selectBar}>
+                {countries.map(country => <div style={styles.button} onClick={() => highlight(country)}>{country}</div>)}
+            </div>
+            <div style={styles.list}>
+                {profiles.map(profile => <Profile
+                                            firstName={profile.firstName}
+                                            lastName={profile.lastName}
+                                            img={profile.img}
+                                            country={profile.country}
+                                            isStudent={profile.isStudent}
+                                            highlight={profile.country === countryHighlight}
+                                            />)}
+            </div>
         </div>
     );
 }
+
+// Version 1 (from iteration 13) below
+// function Facebook() {
+//     const styles = {
+//         list: {
+//             display:        "flex",
+//             flexDirection:  "column",
+//             margin:         10,
+//             gap:            10
+//         }
+//     };
+
+//     return (
+//         <div style={styles.list}>
+//             {profiles.map(profile => <Profile
+//                                         firstName={profile.firstName}
+//                                         lastName={profile.lastName}
+//                                         img={profile.img}
+//                                         country={profile.country}
+//                                         isStudent={profile.isStudent} />)}
+//         </div>
+//     );
+// }
 
 export default Facebook;
--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,20 +1,22 @@
 function Profile(props){
     const styles = {
         container: {
-            display:        "flex",
-            border:         "1px solid black",
-            width:          "40vw",
-            gap:            5
+            display:            "flex",
+            border:             "1px solid black",
+            width:              "40vw",
+            gap:                10,
+            padding:            10,
+            backgroundColor:    props.highlight ? "#a3d2e2" : "white"
         },
         picture: {
-            height:         100,
-            width:          "auto",
-            objectFit:      "cover"
+            height:             100,
+            width:              "auto",
+            objectFit:          "cover"
         },
         description: {
-            display:        "flex",
-            flexDirection:  "column",
-            alignItems:     "start"
+            display:            "flex",
+            flexDirection:      "column",
+            alignItems:         "start"
         },
         field: {
             margin: 0
@@ -33,5 +35,43 @@ function Profile(props){
         </div>
     );
 }
+
+// Version 1 from Iteration 13
+// function Profile(props){
+//     const styles = {
+//         container: {
+//             display:        "flex",
+//             border:         "1px solid black",
+//             width:          "40vw",
+//             gap:            10,
+//             padding:        10
+//         },
+//         picture: {
+//             height:         100,
+//             width:          "auto",
+//             objectFit:      "cover"
+//         },
+//         description: {
+//             display:        "flex",
+//             flexDirection:  "column",
+//             alignItems:     "start"
+//         },
+//         field: {
+//             margin: 0
+//         }
+//     };
+
+//     return (
+//         <div style={styles.container}>
+//             <img style={styles.picture} src={props.img} alt="profile" />
+//             <div style={styles.description}>
+//                 <p style={styles.field}><b>First name: </b>{props.firstName}</p>
+//                 <p style={styles.field}><b>Last name: </b>{props.lastName}</p>
+//                 <p style={styles.field}><b>Country: </b>{props.country}</p>
+//                 <p style={styles.field}><b>Type: </b>{props.isStudent ? "Student" : "Teacher"}</p>
+//             </div>
+//         </div>
+//     );
+// }
 
 export default Profile;
